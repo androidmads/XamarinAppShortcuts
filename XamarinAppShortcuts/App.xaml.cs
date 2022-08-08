56c9c0cbb362071f1830f65d85e440dfdf7b2f3b
Mushtaq M A <mushtaqat3gb@gmail.com>
2022-08-08 22:43:41 +0530

    Add project files.

---
+++ XamarinAppShortcuts/App.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamarinAppShortcuts
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            AppActions.OnAppAction += AppActions_OnAppAction;
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+        void AppActions_OnAppAction(object sender, AppActionEventArgs e)
+        {
+            if (Current != this && Current is App app)
+            {
+                AppActions.OnAppAction -= app.AppActions_OnAppAction;
+                return;
+            }
+            MainThread.BeginInvokeOnMainThread(async () =>
+            {
+                await Shell.Current.GoToAsync($"//{e.AppAction.Id}");
+            });
+        }
+    }
+}
---
+++ XamarinAppShortcuts/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Essentials;
+using System.Diagnostics;
+
+namespace XamarinAppShortcuts
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void BtnAddShortcutClicked(object sender, EventArgs e)
+        {
+            try
+            {
+                await AppActions.SetAsync(
+                    new AppAction("app_info", "App Info", icon: "info"),
+                    new AppAction("app_notifications", "App Notifications", icon: "notification"));
+            }
+            catch (FeatureNotSupportedException ex)
+            {
+                Debug.WriteLine("App Actions not supported");
+            }
+        }
+    }
+}
